fe87c4e6d942b57b233a0dd5882ceb12b1dda22a
서동휘 <seodonghwi@seodonghwiui-MacBookPro.local>
2026-07-17 23:43:54 +0900

    Css 2

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -5,7 +5,9 @@ import styles from "./Layout.module.css"
 const Layout = ({ children }) => {
     return(
     <div className={styles.container}>
-        <Link to= "/">
+        <Link 
+            to= "/"
+            className={styles.containerLink}>
             <h1>Seo Mall</h1>
         </Link>
         <Outlet/>
